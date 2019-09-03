5d3f2ff6c82a7f0d77f9eb863231784460d9431a
rubenbc7 <rubenborbollacastelo@hotmail.com>
2019-09-03 12:26:35 -0700

    cambios

---
+++ app.js
@@ -0,0 +1,30 @@
+const buttons = document.querySelectorAll('button');
+
+const playnote =() => {
+    const button = event.target;
+    const note = button.dataset.note;
+    const audioId = `audio${note}`;
+
+    console.log(audioId);
+    const audio = document.getElementById(audioId);
+    console.log(audio);
+    audio.pause();
+    audio.currentTime = 0;
+    audio.play();
+    
+}
+
+buttons.forEach(
+    button => 
+    button.addEventListener('click', playnote));
+
+     const keyNoteDown = event => {
+         console.log(event);
+         const key = event.key;
+         console.log(key);
+         const button = document.querySelector(`button[data-key="${key}"]`);
+         if(button) button.click();
+     } 
+
+    document.addEventListener('keydown', keyNoteDown);
+
